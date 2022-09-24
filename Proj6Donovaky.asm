e9d02bb17e068877ef58bde653b92b9b9b99e6c3
Kyle <kylemorgandonovan@gmail.com>
2022-09-24 12:01:49 -0400

    Fully Functional String Primitive and Macros program which converts strings to integers via ASCII and then returns these integers to a string and displays them to the user. Includes PROCs for calculation of sum, running sum, and the illustrious truncated average

--- Proj6Donovaky.asm
+++ Proj6Donovaky.asm
@@ -1,11 +1,15 @@
 TITLE  String Primitives and Macros       (Proj6Donovaky.asm)
 
 ; Author: Kyle Donovan
-; Last Modified: 6/1/2022
+; Last Modified: 6/5/2022
 ; OSU email address: donovaky@oregonstate.edu
 ; Course number/section:   CS271 Section 400
-; Project Number: 6                Due Date: 6/8/2022
-; Description:   Im going to use this program to take numbers and calc the average and stuff because why would i have a calculator
+; Project Number: 6                Due Date: 6/5/2022
+; Description:   Program which converts strings to integers 
+;	via ASCII and then returns them to a string and displays them
+;	and calculates and returns the sum of the numbers and the truncated average
+;	also includes the running sum from extra credit
+
 
 INCLUDE Irvine32.inc
 
@@ -14,20 +18,20 @@ mGetString MACRO prompt, userInput, maxCharEnt, strCharCount
 	
 	push		EDX
 	push		ECX
-	push		EBX ;;this can be removed
+	push		EBX				 
 	push		EAX
 
 	mdisplayString	prompt
 
-	mov			EDX, userInput   ;; 12?? --> this is the users input
-	mov			ECX, maxCharEnt	;; this is the length of the users input
+	mov			EDX, userInput   ;; 12 --> this is the users input
+	mov			ECX, maxCharEnt	 ;; this is the length of the users input
 
 	call		ReadString
 
 	mov			strCharCount, EAX
 
 	pop			EAX
-	pop			EBX ;;this can be removed
+	pop			EBX				
 	pop			ECX
 	pop			EDX
 
@@ -45,8 +49,8 @@ ENDM
 
 LO		= -2147483648
 HI		= 2147483647
-MAX		= 21 ;; this is the max size i will accept from a string unless it needs to be 4x this
-TEN		= 10 ;;this is how many numbers i will need to store
+MAX		= 21					
+TEN		= 10					;;this is how many numbers needed to be stored
 
 .data
 welcome_1	BYTE	"String Primitives and Macros! Programmed by Kyle Donovan", 0
@@ -54,144 +58,216 @@ purpose_1	BYTE	"Please provide 10 signed decimal integers. Each number needs to 
 purpose_2	BYTE	"After you have finished inputting the raw numbers I will display a list of the integers, their sum, and their average value.", 0
 get_prompt	BYTE	"Please enter a signed integer: ", 0
 error		BYTE	"ERROR: You did not enter a signed number or your number was too big. ", 0
+extra1		BYTE	"Extra Credit: 1 - I print the numbers the user inputted as a running sum. "
 
-usrArray	SDWORD	TEN DUP(?) ;;change to byte?
-userInput	BYTE	MAX DUP(?)
+usrArray	SDWORD	TEN DUP(?) 
+userInput	BYTE	MAX DUP(0)
+
+addNum		SDWORD	1 DUP(?)
+
 byteCount	DWORD	?
 retry		BYTE	"Please try again. ", 0
 
+
 goodbye		BYTE	"And so marks our fateful end. Adeiu!", 0
-party		BYTE	"woooooo",0
+strDisplay	BYTE	"Here are your numbers: ", 0
+truncAvg	BYTE	"Your truncated average is: ", 0
+summa		BYTE	"The sum is: ", 0
+runningSum	SDWORD	0
+currentSum	BYTE	"Your running sum is: ", 0
+
 num_count	DWORD	10
-negNum		BYTE	"This num is negative", 0
-regularNum	BYTE	"This num is NOT negative", 0
+
+stringHold  BYTE	11 DUP(?)	
 
 AlStorage	DWORD	?
-positivity  BYTE	"This has aaa +", 0
 
+spaceComma	BYTE	", ", 0
+numStore	SDWORD	?
 
-; (insert variable definitions here)
+theSum		SDWORD	?
+trunc8dAvg	SDWORD	?
+
+counterino	DWORD	?
+
 
 .code
 
 ; ----------------------------------------------------------------------------------------------------------
 ; Name: main
 ;
-; Calls Randomize to start, then:
-; Calls the procedures in the order of: introduction, fillArray, displayList, displayMedian, countList, and farewell
+; Calls the procedures in the order of: introduction, readVal, writeVal, 
 ;
-; Preconditions: Proj5_Donovaky.asm is added to our Visual Studio project folder
+; Preconditions: Proj6_Donovaky.asm is added to our Visual Studio project folder
 ;
-; Postconditions: The program generates a random array, displays array, sorts array, calculates and displays median, displays sorted list, and displays counts of each number above 10
+; Postconditions: Converts, displays some numbers that the user put in
 ;
 ; Receives: 
-;		introduction	= introduce our program title, name, extra credits, and instructions for our user
-;		fillArray		= fills our array with random integers
-;		sortList		= orders our list from least to greatest
-;		exchangeElements= a subprocedure of sortList which exchanges the values if they are not in order
-;		displayMedian	= displays the median of our list
-;		displayList		= displays the list, whether sorted or unsorted or as a count measure
-;		countList		= counts the number of items that appear in our list
-;		farewell		= bids our user adieu
+;		
+;		readVal			= reads the input from our user 
+;		writeVal		= writes the values after conversion
 ;
-; Returns: none
+; Returns: a converted integer
 ; ----------------------------------------------------------------------------------------------------------
 
 main PROC
-
-	;; calls introduction which posts a welcome message to our user and explains the purpose of the program
-	push	OFFSET welcome_1 ;; + x
-	push	OFFSET purpose_1 ;; + x
-	push	OFFSET purpose_2 ;;+x
-	call	introduction
+	
+	;; calls introduction which posts a welcome message 
+	;;to our user and explains the purpose of the program
+	mDisplayString OFFSET welcome_1
+	call	CrLf
+	mDisplayString OFFSET purpose_1
+	call	CrLf
+	mDisplayString OFFSET purpose_2
+	call	CrLf
+	mDisplayString OFFSET extra1
+	call	CrLf
 
 	mov		ECX, TEN
 	mov		EDI, OFFSET usrArray
 
 _UserArray:
 	
-	push	OFFSET retry	;; 28
-	push	OFFSET error	;; 24
-	push	OFFSET byteCount ;;20
-	push	OFFSET SIZEOF userInput ;;16
-	push	OFFSET userInput		;;12
-	push	OFFSET get_prompt ;;+8
+	push	OFFSET numStore			;; 32
+	push	OFFSET retry			;; 28
+	push	OFFSET error			;; 24
+	push	OFFSET byteCount		;; 20
+	push	SIZEOF userInput		;; 16
+	push	OFFSET userInput		;; 12
+	push	OFFSET get_prompt		;; +8
 	call	ReadVal
 	
-	mov		[EDI], EAX
+
+	mDisplayString	OFFSET currentSum
+
+	mov		EDX, numStore
+	mov		[EDI], EDX
+	add		runningSum, EDX
+	push	runningSum
+	call	WriteVal
+
+	call	CrLf
+	
 	add		EDI, 4
 	loop	_UserArray
+
 
 	;; we get the number as a string from the user
 	;push	OFFSET get_prompt
 	;call	mGetString
+	call	CrLf
+	mDisplayString OFFSET strDisplay
+
+	mov		ECX, TEN ;;restore my loop value
+	mov		EBX, 0
+	mov		ESI, OFFSET usrArray
+
+	
+_Writing:
+	
+	mov		EAX, [ESI]
+	add		EBX, EAX	
 
 
-	;; displays goodbye message
-	;push	OFFSET goodbye
-	;call	farewell
+	push	OFFSET stringHold ;;12
+	push	EAX				  ;;8
 
+	call	WriteVal
+
+	add		ESI, 4
+	cmp		ECX, 1
+	je		_ImGonnaJump
+	mDisplayString OFFSET spaceComma
+
+_ImGonnaJump:
+	loop	_Writing
+
+
+_SummaMain:	
+	;; sums the nums
+	mov		EAX, EBX
+
+	call	CrLF
+	call	CrLF
+
+	mov		theSum, EBX
+	push	OFFSET summa
+	mDisplayString OFFSET summa
+	push	theSum
+	call	WriteVal
+
+
+	mov		EBX, TEN
+	cdq
+	idiv	EBX
+
+	mov		trunc8dAvg, EAX		;;trun8dAvg stores my truncated average but more efficiently
+
+	call	CrLf
+	call	CrLf
+
+
+	push	OFFSET truncAvg
+	mDisplayString OFFSET truncAvg
+	push	trunc8dAvg
+	call	WriteVal
+
+
+	call	CrLf
+	call	CrLf
+	mDisplayString OFFSET goodbye
 
 	Invoke ExitProcess,0	; exit to operating system
 main ENDP
 
+
 ; ----------------------------------------------------------------------------------------------------------
-; Name: introduction
+; Name: readVal PROC
 ; 
-; ; Introduces the user to our program, explains what our program does
+; readVal converts our users inputted string into an int
 ;
-; Preconditions: None
+; Pre-Condtions: User must enter a number (it does not necessarily need to be valid, but to be converted it does)
 ;
-; Postconditions: changes register EDX
+; Post-Conditions:  returns a converted int 
 ;
-; Receives: 
+; Receives: [EBP+32]		= numStore, a variable to store the number so we can place it in our array later
+;			[EBP+28]		= retry, an encouraging message for our user to try again
+;			[EBP+24]		= error, an error message string which is displayed if our user violates our number bounds
+;			[EBP+20]		= byteCount, a count of our user's bytes
+;			[EBP+16]		= SIZEOF userInput, the size of our user's input in bytes
+;			[EBP+12]		= user_input, a string we receive from our user's input value
+;			[EBP+8]			= get_prompt, a string which displays a request for our user to enter a number
 ;
-; Returns: None
+; Returns: returns a converted int in the bounds of our defined parameters
 ; ----------------------------------------------------------------------------------------------------------
-
-introduction PROC
-
-	push	EBP
-	mov		EBP,ESP
-	push	EDX
-	
-	call	CrLf
-	;;string which communicates who we are and program title
-	mDisplayString [EBP+16]
-	call	CrLf
-	call	CrLf
-	;; welcome user string (1 of 2)
-	mDisplayString [EBP+12]
-	call	CrLf
-	mDisplayString [EBP+8]	;; welcome user string (2 of 2)
-	call	CrLf
-	call	CrLf
-	pop		EDX
-	pop		EBP
-	ret		12
-
-introduction ENDP
 
 ReadVal PROC
-	push	EBP
-	mov		EBP, ESP
-	push	EAX
-	push	EBX
-	push	ECX
-	push	EDX
-	;push	EDI
+	LOCAL negativeTrue: DWORD
+	LOCAL storeVar: DWORD
+
+	push	EDI
+	pushad
+
 	push	ESI
+	mov		EAX, 0 ;;our numInt is set to zero
+
+	mov		negativeTrue, 0
 
 _ReadValChkPt:
 	;;prompt, input, max char entered, string char count
 	mGetString	[EBP+8], [EBP+12], [EBP+16], [EBP+20]
-
 	mov		ESI, [EBP+12]
+	mov		EBX, 0
+	mov		ECX, 0
+	mov		EDX, 0
 	mov		EBX, [EBP+20]
-
+	cld
 	cmp		EBX, 11
 	jg		_Error
-	jmp		_Checker
+	mov		EBX,[EBP+12]
+
+	mov		EBX, [EBP+20]
+	jmp		_SignChecker
 
 _Error:
 ;; this is the please try again message
@@ -201,169 +277,185 @@ _Error:
 	jmp		_ReadValChkPt
 	;; prompt, input, max char entered, string char count
 
-_Checker:	
+_SignChecker:	
 	;; i want to use this spot to check EACH value of my string
-
-	cld
-
-	lodsb	
 	
 	;; need to convert substring to ASCII?
-
+	lodsb
 	cmp		AL, 45
 	je		_NegativeZone
 	cmp		AL, 43
 	je		_ExplicitlyPositive
-	jne		_TheOtherTestingZone
-
-	;;; check if its a number-  if it is a number its all good code continues
-	;;; if its a negative, we need to send it somewhere else - probably also needs one for +
-	;;; if its not a number and not negative, send it back to _Error
 
 
+	cmp		EBX, 0
+	jne		_NumberChecker
 
-_TheOtherTestingZone: ;;;this tests the number
-	;mov		EDX, OFFSET regularNum
-	;call	WriteString
-	;;check if first val is a number
-	lodsb
+_NumberChecker:
 	cmp		AL,48
 	jl		_Error ;; if below 1 range, error
 	cmp		AL, 57
 	jg		_Error	;; if above 0 range, error
 	
-	;;basically this should decrease our false loop as we check each value
-	;; so we need to store our value of the size into a var and decrement it
 
+	sub		AL, 48 ;;this is needed for accuracy/conversion
 
-	;dec		EBX we can decrement in the number converter 
-	;; this should probbaly jump to the conversion, which should jump back to TheOtherTestingZone
-	;;jg		_TheOtherTestingZone
+	dec		EBX
+	cmp		EBX, 0
 
-	;;push	ECX
-	jg		_TheOtherTestingZone
-
+	jmp		_Conversion
 	jmp		_End
 
+_ReloadCheckpoint:
+	lodsb
+	cmp		EBX, 0
+	jne		_NumberChecker
+	jmp		_End
+	;;; check if its a number-  if it is a number its all good code continues
+	;;; if its a negative, we need to send it somewhere else - probably also needs one for +
+	;;; if its not a number and not negative, send it back to _Error
+
+_Conversion:
+	imul	EDX, 10 ;;this is the 10 * EAX part
+
+	jo		_Error
+
+
+	movzx	ECX, AL
+	add		EDX, ECX
+
+	jo		_Error
+	jmp		_ReloadCheckPoint
+
 _NegativeZone: 
-	;mov		EDX, OFFSET negNum
-	;call	WriteString
-	mov		EDX, 1
+
+	lodsb
+	mov		negativeTrue, 1
+
 	dec		EBX
-	jmp		_TheOtherTestingZone
+	jmp		_NumberChecker
 
-;;check if second fval is a number
-
-;_TheItIsANumberZone:
 
 _ExplicitlyPositive:
 	dec		EBX
+	lodsb
 	;mov		EDX, OFFSET positivity
 	;call	WriteString
-	jmp		_TheOtherTestingZone
+	jmp		_NumberChecker
 
 
-;_setNeg:
-	;; neg ##whatever our number string is
-;	jmp		_EndChkPt
+_setNeg:
+	neg		EDX
+	cmp		EDX, LO
+	jl		_Error
+	mov		EAX,[EBP+32]
+	mov		[EAX], EDX
 
-_End:
-;	cmp		EDX, 1
-;	je		_setNeg
+	jmp		_NegativeEnd
+
+_End: ;;this basically checks if user input was negative
+	cmp		negativeTrue, 1
+	je		_setNeg
 
 _EndChkPt:
+	;;;this is where we push the eDX value into our array
 
+	mov		EAX, [EBP+32]
+	mov		[EAX], EDX
+
+_NegativeEnd:
+	;;pop stack
 	pop		ESI
-	;pop		EDI
-	pop		EDX
-	pop		ECX
-	pop		EBX
-	pop		EAX
-	pop		EBP
+	popad
+	pop		EDI
 	ret		24
 
 ReadVal ENDP
 
 
-farewell PROC
-	push	EBP
-	mov		EBP, ESP
+; ----------------------------------------------------------------------------------------------------------
+; Name: writeVal PROC
+; 
+; writeVal procedure to turn our converted integers into hexadecimal/ASCII
+;
+; Pre-Condtions: Only works if the user has entered 10 acceptable values.
+;
+; Post-Conditions: Nothing is changed
+;	Don't read the following line if you don't have a sense of humor, (its just a pun for fun) 
+;	I'm pushing and I'm popping a cap in your class.  
+;
+; Receives: [EBP+12]	= strinHold - a way to store the string
+;			[EBP+8]     = my converted value from my usrArray - its an individual value
+;
+; Returns: displays an ASCIII'd value (the 3 Is are not a typo, its how we ASCIII the final value)
+; ----------------------------------------------------------------------------------------------------------
 
-	mov		EDX, [EBP+8]
-	call	WriteString
-	call	CrLf
+WriteVal PROC
+	LOCAL	negativeTrue: DWORD
+	pushad	
+	mov		negativeTrue, 0
+	mov		ECX, 0
+	
+	mov		EDI, [EBP+12]		;; string holder
+	mov		EAX, [EBP+8]		;; converted value
+	cmp		EAX, 0
+	jl		_negativeMaker		;;checks if our number is negative
+	jmp		_divisionLoop
 
-	pop		EBP
-	ret		4
+_negativeMaker:
+	neg		EAX
+	mov		negativeTrue, 1
+							
 
-farewell ENDP
+_divisionLoop:
+	mov		EDX, 0
+	mov		EBX, 10
+	div		EBX					;;;switched from idiv to div so no cdq
+	
+	cmp		EAX, 0
+	je		_Asciiinator
+	add		EDX, 48
+	push	EDX					;;preserves our register
+	inc		ECX
+	jmp		_divisionLoop
+
+_Asciiinator:
+
+	add		EDX, 48					;;this converts our final value of the number
+	push	EDX
+	inc		ECX
+
+	cmp		negativeTrue, 0
+	
+	jg		_Negator
+	je		_pushString
+
+_Negator:
+	mov AL, 45
+	call	WriteChar
+
+_pushString:
+									;;adds the ascii values into the string
+	pop		EAX
+	stosb
+
+	loop	_pushString
 
 
+_FirstEnd:
+	xor		EAX, EAX				;;i've been told xor is a valuable tool in exploits
+	stosb
+
+	mDisplayString [EBP+12]
+
+_SecondEnd:
+	
+	mov		negativeTrue, 0
+	popad
+
+	ret		8
+
+
+WriteVal ENDP
 
 END main
-
-
-
-	;;this should be user input
-;	mov		ESI, [EBP+12]
-
-	;;this should be the size of the user input but it isnt actually its 10 i think
-;	mov		EBX, [EBP+16]
-
-;	cmp		EBX, 11
-;	jg		_Error
-
-;	jmp		_End
-
-
-;_Error:
-	;; this is the error message
-;	mDisplayString [EBP+24]
-;	call	CrLf
-	;; this is the please try again message
-;	mDisplayString [EBP+28]
-;	call	CrLf
-
-	;; prompt, input, max char entered, string char count
-;	mGetString	[EBP+8], [EBP+12], [EBP+16], [EBP+20]
-
-
-;_End:
-;	mDisplayString [EBP+32]
-
-
-;_Conversion:
-	;;; i have my number is AL = > i want to convert this number into an int because my number is a string
-	;;; for how many numbers are in this, i should convert for that many times. 
-
-	;; conversion time!!!
-	;; numInt = 10 * numInt + (numChar-48)
-
-
-	;; push ECX
-
-;	imul	ECX, 10
-;	mov		ECX, EAX
-	
-;	push	EAX
-
-;	sub		AL, 48
-
-	;; the ECX should be the places, the EAX should be the AL sub 48 = INT
-;	add		ECX, EAX
-
-	;;;need to get imul 10,ECX to be 0, 1, 10, 100, ETC
-
-;	cmp		ECX, 1
-;	jl		_IncECX
-
-;	pop		EAX
-
-;	jmp		_TheOtherTestingZone
-	;;if ecx == 1:
-	;; jump to inc ecx land
-
-;_IncECX:
-	
-;	inc		ECX ;;dk bout this
-;	jmp		_TheOtherTestingZone
